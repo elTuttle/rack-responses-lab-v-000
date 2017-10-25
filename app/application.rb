0ec6b209b94e6728e52fbec775ea0d9ab7fc1219
John Tuttle <thejwt@gmail.com>
2017-10-25 22:33:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,15 @@ class Application
   end
 
   def all(time)
-    
+    resp = Rack::Response.new
+
+    if time < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
   end
 
 end
